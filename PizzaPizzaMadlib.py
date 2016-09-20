e038c4192cc3cd0f0be38a6b26028590d7afaa5b
Will Schmidt <willmart0825@me.com>
2016-09-20 12:58:54 -0400

    Create PizzaPizzaMadlib.py

---
+++ PizzaPizzaMadlib.py
@@ -0,0 +1,11 @@
+Small Madlib about a Pizza Chef. (If you want more Madlibs, just comment below this one, I made this fast for practice but, I can make more.)
+by Will Schmidt
+
+a = input("Adjective? ")
+b = input("Nationality? ")
+c = input("Person? ")
+d = input("Noun? ")
+e = input("Adjective? ")
+f = input("Noun? ")
+
+print ("Pizza was invented by a", a, b, "chef named", c+". To make a pizza, you need to taka lump of", d, "and make a thin round", e, f+".")

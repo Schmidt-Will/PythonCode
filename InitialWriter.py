92953022c1f232f3160e88f0bdcf9a631ea852fe
Will Schmidt <willmart0825@me.com>
2016-09-23 12:58:21 -0400

    Create InitialWriter.py

---
+++ InitialWriter.py
@@ -0,0 +1,9 @@
+Initial Writer
+by Will Schmidt
+
+firstName = input("What is your first name?")
+middleName = input("What is your middle name?")
+lastName = input("What is your last name?")
+
+
+print (firstName[0] + "." + middleName[0] + "." + lastName[0] + ".")
